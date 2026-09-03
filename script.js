df46c8f9859609600765caa49dd6ad9f3c3bfd46
deena <mdeena910@gmail.com>
2026-09-03 13:45:17 +0530

    updated  function(search product)

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+var inputbox=document.getElementById("searchbox");
+var products=document.querySelectorAll(".product");
 
+
+
+
+//search product
+inputbox.addEventListener("input", function(){
+	
+	let inputtext=inputbox.value.toLowerCase();
+	
+	products.forEach(function(product){
+		
+		let productname=product.querySelector("p").textContent.toLowerCase();
+	
+		if(productname.includes(inputtext)){
+			product.style.display="block";
+		}
+			
+		else{
+			
+			product.style.display="none";
+		}
+
+    });
+
+});
